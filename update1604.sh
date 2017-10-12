753c5a7936522bc3de3f4be76e95544462473882
Shafqat Ahmed <shafqatahmed@gmail.com>
2017-10-13 05:46:25 +0600

    changes

--- update1604.sh
+++ update1604.sh
@@ -22,62 +22,73 @@
 #sudo n stable
 
 
-git config --global user.email "shafqatahmed@gmail.com"
-git config --global user.name "Shafqat Ahmed"
+#git config --global user.email "shafqatahmed@gmail.com"
+#git config --global user.name "Shafqat Ahmed"
+
+# Install JDK
+#sudo apt-get install default-jre
+#sudo apt-get install default-jdk
+
+######################ORACLE JDK 
+#sudo apt-get install python-software-properties
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install oracle-java8-installer
+#cd ~/
+#echo JAVA_HOME=/usr/lib/jvm/java-8-oracle >> .bashrc
+#echo export JAVA_HOME >> .bashrc
+#echo 'JAVA_HOME=/usr/lib/jvm/java-8-oracle' | sudo tee --append /etc/environment > /dev/null
+#echo 'export JAVA_HOME' | sudo tee --append /etc/environment > /dev/null
+######################################################
+
+#sudo apt-get install mc doublecmd-gtk gparted conky conky-all -y
+## Install Titillioum Font
+#cd ~/Downloads
+#wget http://www.fontsquirrel.com/fonts/download/Titillium
+#mv Titillium Titillium.zip
+#unzip Titillium.zip -d Titillium
+#sudo cp ./Titillium/*.otf /usr/share/fonts/ 
+
+# Install Wine 
+#sudo apt-get install wine
+
+# Install VS Code
+#sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+#sudo apt-get update && sudo apt-get install ubuntu-make
+#umake web visual-studio-code
+
+umake ide webstorm
+umake ide pycharm-professional
+umake ide idea-ultimate
+umake go go-lang
+umake ide eclipse 
+
 
 exit 
 
-# Install JDK
-
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-
-cd ~/
-echo JAVA_HOME=/usr/lib/jvm/java-8-oracle >> .bashrc
-echo export JAVA_HOME >> .bashrc
-echo 'JAVA_HOME=/usr/lib/jvm/java-8-oracle' | sudo tee --append /etc/environment > /dev/null
-echo 'export JAVA_HOME' | sudo tee --append /etc/environment > /dev/null
 
 # Install android SDK tools
-cd ~/Downloads 
-wget http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz
-tar zxvf android-sdk_r24.1.2-linux.tgz
-sudo mv android-sdk-linux /opt/google/android
-cd ~/
-echo ANDROID_HOME=/opt/google/android >> .bashrc
-echo export ANDROID_HOME >> .bashrc
-echo 'ANDROID_HOME=/opt/google/android' | sudo tee --append /etc/environment > /dev/null
-echo 'export ANDROID_HOME' | sudo tee --append /etc/environment > /dev/null
+#cd ~/Downloads 
+#wget http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz
+#tar zxvf android-sdk_r24.1.2-linux.tgz
+#sudo mv android-sdk-linux /opt/google/android
+#cd ~/
+#echo ANDROID_HOME=/opt/google/android >> .bashrc
+#echo export ANDROID_HOME >> .bashrc
+#echo 'ANDROID_HOME=/opt/google/android' | sudo tee --append /etc/environment > /dev/null
+#echo 'export ANDROID_HOME' | sudo tee --append /etc/environment > /dev/null
 
 # Ionic and cordova
-sudo npm install cordova -g
-sudo chown -R shafqat /usr/local/lib/node_modules/
-sudo npm install cordova -g
-sudo npm install ionic -g
+#sudo npm install cordova -g
+#sudo chown -R shafqat /usr/local/lib/node_modules/
+#sudo npm install cordova -g
+#sudo npm install ionic -g
 
 # Add various ppas
 
 
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install virtualbox-4.3 dkms
-
-sudo apt-get install -y mongodb-org
-sudo service mongod start
-
-
-sudo apt-get install doublecmd-gtk
-cd ~/Downloads
-wget https://vivaldi.com/download/Vivaldi_TP3.1.0.162.4-1_amd64.deb
-
-
-sudo apt-get install ant
-sudo ln /opt/google/android/tools/android /usr/bin/android
-sudo apt-get install gparted
-sudo apt-get install conky conky-all
+#sudo ln /opt/google/android/tools/android /usr/bin/android
+#sudo apt-get install conky conky-all
 
 
 # Themes & Icons
