6c54424f85456ab1cb86096cc484f8884c5547aa
Shafqat Ahmed <shafqatahmed@gmail.com>
2017-10-16 07:07:25 +0600

    Update Final

--- update1604.sh
+++ update1604.sh
@@ -1,11 +1,11 @@
 
 
-updateLinux () {
+ub-updateLinux () {
     sudo apt-get update
     sudo apt-get dist-upgrade
 }
 
-installChrome() {
+ub-installChrome() {
     # Chrome
     cd /tmp
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -14,13 +14,13 @@ installChrome() {
     sudo apt-get -f install
 }
 
-installToolset1() {
+ub-installToolset1() {
     sudo apt-get install unity-tweak-tool openssh-server git mc -y 
     git config --global user.email "shafqatahmed@gmail.com"
     git config --global user.name "Shafqat Ahmed"
 }
 
-installNodeJs(){
+ub-installNodeJs(){
     # Install Node
 
     sudo apt install nodejs npm -y
@@ -33,13 +33,13 @@ installNodeJs(){
 }
 
 
-installOpenJdk() {
+ub-installOpenJdk() {
     # Install JDK
     sudo apt-get install default-jre -y
     sudo apt-get install default-jdk -y
 }
 
-installOracleJdk() {
+ub-installOracleJdk() {
     ######################ORACLE JDK 
     sudo apt-get install python-software-properties -y
     sudo add-apt-repository ppa:webupd8team/java -y
@@ -53,11 +53,11 @@ installOracleJdk() {
     ######################################################
 }
 
-installToolSet2(){
+ub-installToolSet2(){
     sudo apt-get install mc doublecmd-gtk gparted conky conky-all terminator -y
 }
 
-installFonts() {
+ub-installFonts() {
     ## Install Titillioum Font
     cd ~/Downloads
     wget http://www.fontsquirrel.com/fonts/download/Titillium
@@ -67,13 +67,13 @@ installFonts() {
 }
 
 
-installWine() {
+ub-installWine() {
     # Install Wine 
     sudo apt-get install wine
 }
 
 
-installIDEs() {
+ub-installIDEs() {
     # Install VS Code
     sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
     sudo apt-get update && sudo apt-get install ubuntu-make
@@ -87,7 +87,7 @@ installIDEs() {
 }
 
 
-installThemeMacbuntu() {
+ub-installThemeMacbuntu() {
     # Noobslab Macbuntu Theme
     cd /tmp
     wget http://drive.noobslab.com/data/Mac/MacBuntu-Wallpapers.zip
@@ -97,17 +97,47 @@ installThemeMacbuntu() {
     sudo apt-get install macbuntu-os-icons-lts-v7 macbuntu-os-ithemes-lts-v7 -y
 }
 
-installPlayOnLinux() {
+ub-installPlayOnLinux() {
     wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
     sudo wget http://deb.playonlinux.com/playonlinux_trusty.list -O /etc/apt/sources.list.d/playonlinux.list
     sudo apt-get update
     sudo apt-get install playonlinux -y
 }
 
+ub-updateWifiDriverAndKernel () {
+    cd /lib/firmware
+    sudo wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/iwlwifi-8000C-27.ucode
+    cd tmp/
+    mkdir kernels
+    cd /tmp/kernels/
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.13/linux-headers-4.13.0-041300_4.13.0-041300.201709031731_all.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.13/linux-headers-4.13.0-041300-generic_4.13.0-041300.201709031731_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.13/linux-image-4.13.0-041300-generic_4.13.0-041300.201709031731_amd64.deb
+    rm *.deb
+    sudo dpkg -i *.deb
+    sudo reboot
+}
 
+up-installDocker(){
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt install docker-ce -y
+}
 
-exit 
+up-installThemes (){
+    sudo add-apt-repository ppa:ravefinity-project/ppa -y
+    sudo add-apt-repository ppa:snwh/pulp -y
+    sudo add-apt-repository ppa:noobslab/themes -y
+    sudo add-apt-repository ppa:system76/pop -y
+    sudo add-apt-repository ppa:numix/ppa -y 
+    sudo apt-get update
+    sudo apt-get install numix-gtk-theme numix-icon-theme numix-folders  numix-icon-theme-circle  numix-icon-theme-square -y
+    sudo apt-get install flat-remix-gs-theme  ultimate-blue-theme t4g-v2-theme pop-gtk-theme pop-gnome-shell-theme -y
+    sudo apt-get install minwaita-theme vimix-gtk-themes arc-theme paper-gtk-theme paper-icon-theme paper-cursor-theme ambiance-colors radiance-colors -y
+}
 
+return
 
 
 
@@ -149,7 +179,7 @@ sudo apt-get install numix-gtk-theme numix-icon-theme-bevel numix-icon-theme-cir
 sudo apt-get install zukitwo zukiwi mac-ithemes-v3 mac-icons-v3 orchis3 flattastic-suite trevilla-themes vimix-flat-themes emerald-icon-theme nitrux-icon-theme azure-gtk-theme # gnomishbeige-theme gnomishdark-theme gnomishgrey-theme
 sudo apt-get install evolvere-icon-suite
 
-# Play on linux 
+# Play on linux https://go.skype.com/linux.deb
 sudo add-apt-repository ppa:noobslab/apps
 sudo apt-add-repository -y ppa:teejee2008/ppa
 
